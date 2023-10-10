e04bb94935270116aa39ef08008149476ba7ab21
Oghenekparobor Eminokanju <robor.eminokanju@gmail.com>
2023-10-10 16:19:30 +0100

    chore: created the php product object class

--- src/Product.php
+++ src/Product.php
@@ -2,7 +2,63 @@
 
 namespace App;
 
+/**
+ * Product
+ */
 class Product
-{
-
+{    
+    /**
+     * title
+     *
+     * @var mixed
+     */
+    public $title;    
+    /**
+     * price
+     *
+     * @var mixed
+     */
+    public $price;    
+    /**
+     * imageUrl
+     *
+     * @var mixed
+     */
+    public $imageUrl;    
+    /**
+     * capacityMB
+     *
+     * @var mixed
+     */
+    public $capacityMB;    
+    /**
+     * colour
+     *
+     * @var mixed
+     */
+    public $colour;    
+    /**
+     * availabilityText
+     *
+     * @var mixed
+     */
+    public $availabilityText;    
+    /**
+     * isAvailable
+     *
+     * @var mixed
+     */
+    public $isAvailable;    
+    /**
+     * shippingText
+     *
+     * @var mixed
+     */
+    public $shippingText;    
+    /**
+     * shippingDate
+     *
+     * @var mixed
+     */
+    public $shippingDate;
 }
